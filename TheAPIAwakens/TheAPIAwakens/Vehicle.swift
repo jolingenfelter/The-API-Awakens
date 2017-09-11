81dd4b9d4112588eb5ad089e0f0160970de81431
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-09-11 18:06:16 -0400

    Reorganize files

---
+++ TheAPIAwakens/TheAPIAwakens/Vehicle.swift
@@ -0,0 +1,47 @@
+//
+//  Vehicle.swift
+//  TheAPIAwakens
+//
+//  Created by Joanna Lingenfelter on 9/11/17.
+//  Copyright © 2017 JoLingenfelter. All rights reserved.
+//
+
+import Foundation
+
+struct Vehicle {
+    
+    let name: String?
+    let model: String?
+    let costString: String?
+    let lengthString: String?
+    let vehicleClass: String?
+    let crew: String?
+    
+    let lengthDouble: Double?
+    let costDouble: Double?
+    
+}
+
+extension Vehicle: JSONDecodable {
+    
+    init?(JSON: [String : AnyObject]) {
+        name = JSON["name"] as? String
+        model = JSON["model"] as? String
+        costString = JSON["cost_in_credits"] as? String
+        lengthString = JSON["length"] as? String
+        vehicleClass = JSON["vehicle_class"] as? String
+        crew = JSON["crew"] as? String
+        
+        if let vehicleLength = self.lengthString {
+            self.lengthDouble = Double(vehicleLength)
+        } else {
+            self.lengthDouble = 0
+        }
+        
+        if let vehicleCost = self.costString {
+            self.costDouble = Double(vehicleCost)
+        } else {
+            self.costDouble = 0
+        }
+    }
+}
