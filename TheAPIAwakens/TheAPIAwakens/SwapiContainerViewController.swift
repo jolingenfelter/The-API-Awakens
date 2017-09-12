0d2ffd93c24dcdba35ca5db5dd57481cfc5e8062
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-09-12 10:49:31 -0400

    BaseViewController setup

---
+++ TheAPIAwakens/TheAPIAwakens/SwapiContainerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SwapiContainerViewController.swift
+//  TheAPIAwakens
+//
+//  Created by Joanna Lingenfelter on 9/12/17.
+//  Copyright © 2017 JoLingenfelter. All rights reserved.
+//
+
+import UIKit
+
+class SwapiContainerViewController: BaseSwapiController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
