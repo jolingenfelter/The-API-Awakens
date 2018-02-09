cabd98005ce569b0067d8c08bea36728c7ab9bd7
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2018-02-09 17:41:03 -0500

    Animate views on firstLoad only

--- TheAPIAwakens/TheAPIAwakens/HomeViewController.swift
+++ TheAPIAwakens/TheAPIAwakens/HomeViewController.swift
@@ -18,17 +18,25 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var vehiclesCenterX: NSLayoutConstraint!
     @IBOutlet weak var starshipsCenterX: NSLayoutConstraint!
     
+    var firstLoad = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         let constraintsArray = [charactersCenterX, vehiclesCenterX, starshipsCenterX]
-        for constraint in constraintsArray {
-            constraint?.constant = -view.frame.size.width
+        
+        if firstLoad {
+            for constraint in constraintsArray {
+                constraint?.constant = -view.frame.size.width
+            }
+        } else {
+            for constraint in constraintsArray {
+                constraint?.constant = 0
+            }
         }
         
     }
@@ -36,20 +44,24 @@ class HomeViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
-            self.charactersCenterX.constant = 0
-            self.view.layoutIfNeeded()
-        }, completion: nil)
+        if firstLoad {
+            UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
+                self.charactersCenterX.constant = 0
+                self.view.layoutIfNeeded()
+            }, completion: { _ in
+                self.firstLoad = false
+            })
         
-        UIView.animate(withDuration: 2.0, delay: 0.3, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
-            self.vehiclesCenterX.constant = 0
-            self.view.layoutIfNeeded()
-        }, completion: nil)
+            UIView.animate(withDuration: 2.0, delay: 0.3, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
+                self.vehiclesCenterX.constant = 0
+                self.view.layoutIfNeeded()
+            }, completion: nil)
         
-        UIView.animate(withDuration: 2.0, delay: 0.6, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
-            self.starshipsCenterX.constant = 0
-            self.view.layoutIfNeeded()
-        }, completion: nil)
+            UIView.animate(withDuration: 2.0, delay: 0.6, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
+                self.starshipsCenterX.constant = 0
+                self.view.layoutIfNeeded()
+            }, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
