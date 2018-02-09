23c627e7093c802e3726989f5a39ef2c9f753561
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2018-02-09 17:34:57 -0500

    HomeScreen Button Animations

--- TheAPIAwakens/TheAPIAwakens/HomeViewController.swift
+++ TheAPIAwakens/TheAPIAwakens/HomeViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var charactersStackView: UIStackView!
+    @IBOutlet weak var vehiclesStackView: UIStackView!
+    @IBOutlet weak var starshipsStackView: UIStackView!
+    
+    @IBOutlet weak var charactersCenterX: NSLayoutConstraint!
+    @IBOutlet weak var vehiclesCenterX: NSLayoutConstraint!
+    @IBOutlet weak var starshipsCenterX: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let constraintsArray = [charactersCenterX, vehiclesCenterX, starshipsCenterX]
+        for constraint in constraintsArray {
+            constraint?.constant = -view.frame.size.width
+        }
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
+            self.charactersCenterX.constant = 0
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 2.0, delay: 0.3, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
+            self.vehiclesCenterX.constant = 0
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 2.0, delay: 0.6, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .curveEaseOut, animations: {
+            self.starshipsCenterX.constant = 0
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
